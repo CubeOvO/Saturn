3027c92f5139813e46641332962efa27fd4afe1b
Zeqi Zhang <28584357+CubeOvO@users.noreply.github.com>
2018-05-18 16:48:20 +0100

    Update labels, update code structure

--- week2/half_life/half_life_against_distance.py
+++ week2/half_life/half_life_against_distance.py
@@ -10,35 +10,57 @@ import matplotlib.pyplot as plt
 import numpy as sp
 #%%
 distance = sp.linspace(0.1,12,100)
-#%%
 dk = pd.read_csv('summary.csv')
+chem = list(dk['chemical'])
 k_rate = list(dk['k_rate'])
+
+
+# calculate the k_rate for each chemical
+real_krate = []
+for i in range(len(k_rate)):
+	k = distance**-2*k_rate[i]
+	real_krate.append(k)
+print(real_krate)
+
+
 #%%
-realk_rate = []
-for i in range(len(distance)):
-    k = k_rate/(distance[i])**2
-    realk_rate.append(k)
-#%%
+# plot config
 plt.xlabel("Distance(AU)")
 plt.ylabel("Reaction Rate(s^-1)")
 plt.yscale('log')
 plt.xlim(0,13)
 plt.grid()
-plt.plot(distance, realk_rate)
+
+# plot krate for each chemical
+
+plt.title('krate for each chemical')
+for i in range(len(real_krate)):
+	plt.plot(distance, real_krate[i],label=chem[i])
+plt.legend()
+plt.show()
 #%%
+
+# calculate the halflife for each chemical
 halflife = []
-for i in range(len(realk_rate)):
-    h = 1/(realk_rate[i])
-    halflife.append(h)
-#%%
+for i in range(len(real_krate)):
+	k = real_krate[i]**-1
+	halflife.append(k)
+
+# plot
 plt.xlabel("Distance(AU)")
 plt.ylabel("Half life(s)")
 plt.yscale('log')
 plt.ylim(10**-3,10**4)
 plt.grid()
-plt.plot(distance, halflife)
+
+# plot half life for each chemical
+for i in range(len(halflife)):
+	plt.plot(distance, halflife[i],label=chem[i])
+plt.legend()
+#%%
+plt.title(' half life for each chemical')
+# plot earth, jupiter, Saturn
 plt.plot(sp.linspace(1,1,100),sp.linspace(10**-3,10**4,100),color='grey')
 plt.plot(sp.linspace(5.2,5.2,100),sp.linspace(10**-3,10**4,100),color='grey')
 plt.plot(sp.linspace(9,9,100),sp.linspace(10**-3,10**4,100),color='grey')
-
-plt.legend(, )
+plt.show()
